73ce0f8a682ee5562a6161c6c97f2834f5d864b2
rexdavinci <whomiwish2@gmail.com>
2019-03-13 19:31:11 +0700

    controlled and uncontrolled inputs

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,69 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import '../../resources/sass/App.scss'
 
-
-const Person = ({img,name,age, info}) => {
-  return(
-    <div>
-      <img src={img} alt="person"/>
-      <h2>name: {name}</h2>
-      <h2>age: {age}</h2>
-      <h5>info: {info}</h5>
-    </div>
-  )
-}
-
-Person.propTypes = {
-  img: PropTypes.string.isRequired,
-  age: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  info: PropTypes.string.isRequired
-}
-Person.defaultProps = {
-  info: "No info available"
-}
-class PersonList extends Component{
+class ControlledInputs extends Component{
   state={
-    people:[
-      {
-        id: 1,
-        img: "https://randomuser.me/api/portraits/thumb/men/24.jpg",
-        name: "John",
-        age: 31,
-        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, architecto?"
-      },
-      {
-        id: 2,
-        img: "https://randomuser.me/api/portraits/thumb/men/32.jpg",
-        name: "Bob",
-        age: 32
-      }
-    ]
+    text:''
+  }
+  handleChange= e=> {
+    const name = e.target.name
+    const value = e.target.value.toUpperCase()
+
+    this.setState({
+      text: value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state.text.toUpperCase())
   }
   render(){
     return(
-      <div>
-        {this.state.people.map(person=> {
-          return(
-            <Person key={person.id} name={person.name} img={person.img} age={person.age} info={person.info}/>
-          )
-        })}
-      </div>
+      <form style={{margin: "3rem"}} onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} value={this.state.text} type="text" name="firstname"/>
+        <button type="submit">Submit</button>
+      </form>
     )
   }
 }
+
+
+
+class UncontrolledInputs extends Component{
+  handleSubmit= (e) => {
+    e.preventDefault()
+    console.log(this.firstname.value)
+    console.log(this.text.textContent)
+  }
+  render(){
+    return(
+      <form style={{margin: "3rem"}}>
+        <input type="text" name="firstname" ref={(item)=>this.firstname =item}/>
+        <button onClick={this.handleSubmit}type="submit">Submit</button>
+        <p ref={(input)=>this.text = input}>Hello World</p>
+      </form>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <PersonList/>
+        <ControlledInputs/>
+        <hr/>
+        <UncontrolledInputs />
       </div>
     )
   }
